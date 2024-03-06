171f3289cfaf13d8440a678908cd9ebb78257815
Tarika <patankartarika@gmail.com>
2024-03-06 06:22:04 +0530

    Adding boobookTicket.js

---
+++ bookTicket.js
@@ -0,0 +1,16 @@
+//Booking Flight Ticket
+
+// let ticket = "booked";
+let ticket = "Not booked";
+const p1 = new Promise((resovle, reject) => {
+  setTimeout(() => {
+    if (ticket == "booked") {
+      resovle("Your ticket booked successfully");
+    } else {
+      reject(new Error("Your ticket not booked"));
+    }
+  }, 2000);
+});
+
+// console.log(p1);
+p1.then((res) => console.log(res)).catch((err) => console.log(err));
