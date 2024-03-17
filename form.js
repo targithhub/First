1804204e5d9100ff8438a2f8d0ce7b26db952fde
targithhub <146104611+targithhub@users.noreply.github.com>
2024-03-17 20:33:48 +0530

    Adding form and registration form

---
+++ form.js
@@ -0,0 +1,27 @@
+console.log("html form");
+
+const form = document.getElementById("form");
+const username = document.getElementById("uname");
+const pswd = document.getElementById("pwd");
+const msg = document.getElementById("message");
+
+const correctUserName = "root";
+const correctPassword = "Js@123";
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  if (username.value == correctUserName && pswd.value == correctPassword) {
+    let formdata = new FormData(form); //FormData() is predefined class
+    console.log(formdata);
+    const details = {};
+    formdata.forEach((fieldValue, keyName) => {
+      details[keyName] = fieldValue;
+    });
+    console.log(details);
+  } else {
+    msg.innerText = "Incorrect username and Password";
+  }
+}
+
+form.addEventListener("submit", handleFormSubmit);
