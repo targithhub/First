e93578023c83ce868f166e202c42d382b943b2a0
targithhub <146104611+targithhub@users.noreply.github.com>
2024-03-20 17:37:53 +0530

    Adding file box Counter

---
+++ boxCounter.js
@@ -0,0 +1,25 @@
+console.log("counter");
+
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+
+let para = document.getElementById("p1");
+
+para.textContent = 0;
+
+function addOne() {
+  para.textContent = Number(para.textContent) + 1;
+}
+
+function addThree() {
+  para.textContent = Number(para.textContent) + 3;
+}
+
+function addFive() {
+  para.textContent = Number(para.textContent) + 5;
+}
+
+b1.addEventListener("click", addOne);
+// b2.addEventListener("click", addThree);
+// b3.addEventListener("click", addFive);
